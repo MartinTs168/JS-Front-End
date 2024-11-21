f277ed89600a81774260bc9eef822aa3f561daa4
MartinTs168 <martin.tsenev@gmail.com>
2024-11-21 21:27:11 +0300

    finished the tasks for dom exercise

--- dom_ex/09. Number Convertor/number-convertor.js
+++ dom_ex/09. Number Convertor/number-convertor.js
@@ -1,3 +1,21 @@
 function solve() {
-    // TODO
+    const number = Number(document.querySelector('#input').value);
+    const outputEl = document.querySelector('#result');
+    const convertTo = document.querySelector('#selectMenuTo').value;
+
+    switch (convertTo) {
+        case 'binary': {
+            outputEl.value = number.toString(2);
+            break;
+        }
+
+        case 'hexadecimal': {
+            outputEl.value = number.toString(16).toUpperCase();
+            break;
+        }
+
+        default: {
+            outputEl.value = '';
+        }
+    }
 }
