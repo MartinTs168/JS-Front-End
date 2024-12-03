3603ef69e254b467b608b9343cda519f67c4bd14
MartinTs168 <martin.tsenev@gmail.com>
2024-12-03 17:35:12 +0200

    added task for dom events ex

--- dom_events_ex/07. Distance-Converter/distance-converter.js
+++ dom_events_ex/07. Distance-Converter/distance-converter.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const metersToElseValues = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254,
+    };
+
+    const inputEl = document.querySelector('#inputDistance');
+    const outputEl = document.querySelector('#outputDistance');
+
+    const convertButton = document.querySelector('#convert');
+
+    function convert() {
+        const convertFrom = document.querySelector('select#inputUnits').value;
+        const convertTo = document.querySelector('select#outputUnits').value;
+
+        const valInMeters = metersToElseValues[convertFrom] * inputEl.value;
+        console.log(valInMeters);
+        const outputValue = valInMeters / metersToElseValues[convertTo];
+        console.log(outputValue);
+        outputEl.value = outputValue;
+    }
+    convertButton.addEventListener('click', convert);
 }
--- dom_events_ex/08. Sudomu/sudomu.js
+++ dom_events_ex/08. Sudomu/sudomu.js
@@ -1,5 +1,77 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const sizeEl = document.querySelector('#size');
+    const formEl = document.querySelector('#solutionCheck');
+    const ouputEl = document.querySelector('#check');
+
+    let status = '';
+    let size = 3;
+
+    sizeEl.addEventListener('change', (e) => {
+        size = Number(e.target.value);
+    });
+
+    formEl.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const rows = formEl.querySelectorAll('table tbody tr');
+
+        rows.forEach((row) => {
+            const values = [...row.children].map((el) => el.children[0].value);
+
+            const duplicates = values.filter(
+                (item, index) => values.indexOf(item) !== index
+            );
+
+            if (duplicates.length > 0) {
+                status += '0';
+            } else {
+                status += '1';
+            }
+        });
+
+        status += 'x';
+
+        for (let i = 1; i <= size; i++) {
+            const column = [
+                ...e.target.querySelectorAll(
+                    `table tbody tr td:nth-child(${i})`
+                ),
+            ].map((el) => el.children[0].value);
+
+            const duplicates = column.filter(
+                (item, index) => column.indexOf(item) !== index
+            );
+
+            if (duplicates.length > 0) {
+                status += '0';
+            } else {
+                status += '1';
+            }
+        }
+
+        if (status === '111x111') {
+            ouputEl.classList.remove('fail');
+            ouputEl.classList.add('success');
+            ouputEl.textContent = 'Success!';
+        } else {
+            ouputEl.classList.remove('success');
+            ouputEl.classList.add('fail');
+            ouputEl.textContent = 'Keep trying ...';
+        }
+        console.log(status);
+    });
+
+    const inputs = document.querySelectorAll('table input');
+
+    inputs[0].value = 1;
+    inputs[1].value = 2;
+    inputs[2].value = 3;
+    inputs[3].value = 2;
+    inputs[4].value = 3;
+    inputs[5].value = 1;
+    inputs[6].value = 3;
+    inputs[7].value = 1;
+    inputs[8].value = 2;
 }
